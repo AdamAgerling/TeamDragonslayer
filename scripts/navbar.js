12a6c899f44f54ad611af721a7cbe012236db8e3
AdamAgerling <Alltidvakenhehe@gmail.com>
2025-03-18 10:41:11 +0100

    Add sticky-top to navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const currentPath = window.location.pathname;
 
   const navElement = document.createElement("nav");
-  navElement.className = "navbar navbar-expand-lg bg-dark navbar-dark";
+  navElement.className =
+    "navbar navbar-expand-lg bg-dark navbar-dark sticky-top";
   navElement.innerHTML = `
         <div class="container-fluid">
             <a class="navbar-brand text-danger fs-2 d-flex align-items-center" href="/index.html">
